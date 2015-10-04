ce4442b565a7f316714f10f6e54c3d95216fcac1
David Portabella <david.portabella@gmail.com>
2015-10-05 01:24:00 +0545

    testing the three listeners: key pressed, released, typed. can the key class be used to predict whether a key_pressed event will be followed by a key_typed event?

--- src/main/java/keyboard2AndroidUSB/Keyboard2AndroidUSB.java
+++ src/main/java/keyboard2AndroidUSB/Keyboard2AndroidUSB.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class Keyboard2AndroidUSB extends Application {
@@ -14,11 +15,27 @@ public class Keyboard2AndroidUSB extends Application {
         Scene scene = new Scene(new Label(), 300, 275);
         primaryStage.setScene(scene);
 
-        scene.setOnKeyPressed((KeyEvent e) -> System.out.println("key_pressed, code: " + e.getCode()));
-        scene.setOnKeyReleased((KeyEvent e) -> System.out.println("key_released, code: " + e.getCode()));
+        scene.setOnKeyPressed((KeyEvent e) -> System.out.println("key_pressed, code: " + keyCodeToString(e.getCode())));
+        scene.setOnKeyReleased((KeyEvent e) -> System.out.println("key_released, code: " + keyCodeToString(e.getCode())));
         scene.setOnKeyTyped((KeyEvent e) -> System.out.println("key_typed, char: " + e.getCharacter()));
 
         primaryStage.show();
+    }
+
+    String keyCodeToString(KeyCode keyCode) {
+        StringBuffer sp = new StringBuffer();
+        sp.append("[name: " + keyCode.getName() + ", code: " + keyCode.impl_getCode() + ", char: " + keyCode.impl_getChar() + ", class: ");
+        if (keyCode.isFunctionKey()) { sp.append(" function"); }
+        if (keyCode.isNavigationKey()) { sp.append(" navigation"); }
+        if (keyCode.isArrowKey()) { sp.append(" arrow"); }
+        if (keyCode.isModifierKey()) { sp.append(" modifier"); }
+        if (keyCode.isLetterKey()) { sp.append(" letter"); }
+        if (keyCode.isDigitKey()) { sp.append(" digit"); }
+        if (keyCode.isKeypadKey()) { sp.append(" keypad"); }
+        if (keyCode.isWhitespaceKey()) { sp.append(" whitespace"); }
+        if (keyCode.isMediaKey()) { sp.append(" media"); }
+        sp.append("]");
+        return sp.toString();
     }
 
     public static void main(String[] args) { launch(args); }
