561cc6f28dda3f3ad08191ae07a870205215f630
David Portabella <david.portabella@gmail.com>
2015-10-05 02:46:16 +0545

    implemeted a logic based on experimenting, to decide between key_pressed/release and key_typed events; todo: need implementing unit tests, and more experimenting and finding the proper documentation

--- src/main/java/keyboard2AndroidUSB/Keyboard2AndroidUSB.java
+++ src/main/java/keyboard2AndroidUSB/Keyboard2AndroidUSB.java
@@ -7,7 +7,22 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
+/*
+How can I predict whether a key_pressed event will be followed by a key_typed event?
+http://stackoverflow.com/questions/32897191/how-can-i-predict-whether-a-key-pressed-event-will-be-followed-by-a-key-typed-ev
+
+I didn't find doc about this;
+it seems that I cannot decide based on keyCode classes (function, navigation, arrow...)
+I've implemented this logic based on experimenting.
+
+- note that LAST_CHAR_AS_CONTROL_EVENT < LAST_KEY_CODE_AS_CONTROL_EVENT!
+  for instance, typing the Up arrow, produces the KeyCode.UP = 0x26, while typing the char '&' also produces the char 0x26;
+
+- note for instance, the enter (0x0A), the backspace (0x08), the space (0x20) keys produce both a key_pressed/key_released and a key_typed event.
+*/
 public class Keyboard2AndroidUSB extends Application {
+    final int LAST_KEY_CODE_AS_CONTROL_EVENT = KeyCode.DOWN.impl_getCode(); // 0x28
+    final int LAST_CHAR_AS_CONTROL_EVENT = (int) ' '; // 0x20
 
     @Override
     public void start(Stage primaryStage) {
@@ -15,26 +30,38 @@ public class Keyboard2AndroidUSB extends Application {
         Scene scene = new Scene(new Label(), 300, 275);
         primaryStage.setScene(scene);
 
-        scene.setOnKeyPressed((KeyEvent e) -> System.out.println("key_pressed, code: " + keyCodeToString(e.getCode())));
-        scene.setOnKeyReleased((KeyEvent e) -> System.out.println("key_released, code: " + keyCodeToString(e.getCode())));
-        scene.setOnKeyTyped((KeyEvent e) -> System.out.println("key_typed, char: " + e.getCharacter()));
+        scene.setOnKeyPressed((KeyEvent e) -> {
+            if (handleAsControlEvent(e.getCode())) {
+                System.out.println("key_pressed, code: " + keyCodeToString(e.getCode()));
+            }
+        });
+
+        scene.setOnKeyReleased((KeyEvent e) -> {
+            if (handleAsControlEvent(e.getCode())) {
+                System.out.println("key_released, code: " + keyCodeToString(e.getCode()));
+            }
+        });
+
+        scene.setOnKeyTyped((KeyEvent e) -> {
+            if (handleAsTypedEvent(e.getCharacter())) {
+                System.out.println("key_typed, char: " + e.getCharacter());
+            }
+        });
 
         primaryStage.show();
     }
 
-    String keyCodeToString(KeyCode keyCode) {
+    private boolean handleAsControlEvent(KeyCode keyCode) {
+        return (keyCode.impl_getCode() <= LAST_KEY_CODE_AS_CONTROL_EVENT);
+    }
+
+    private boolean handleAsTypedEvent(String c) {
+        return (c.length() > 0 && (c.length() > 1 || c.charAt(0) > LAST_CHAR_AS_CONTROL_EVENT));
+    }
+
+    private String keyCodeToString(KeyCode keyCode) {
         StringBuffer sp = new StringBuffer();
-        sp.append("[name: " + keyCode.getName() + ", code: " + keyCode.impl_getCode() + ", char: " + keyCode.impl_getChar() + ", class: ");
-        if (keyCode.isFunctionKey()) { sp.append(" function"); }
-        if (keyCode.isNavigationKey()) { sp.append(" navigation"); }
-        if (keyCode.isArrowKey()) { sp.append(" arrow"); }
-        if (keyCode.isModifierKey()) { sp.append(" modifier"); }
-        if (keyCode.isLetterKey()) { sp.append(" letter"); }
-        if (keyCode.isDigitKey()) { sp.append(" digit"); }
-        if (keyCode.isKeypadKey()) { sp.append(" keypad"); }
-        if (keyCode.isWhitespaceKey()) { sp.append(" whitespace"); }
-        if (keyCode.isMediaKey()) { sp.append(" media"); }
-        sp.append("]");
+        sp.append("[name: " + keyCode.getName() + ", code: " + keyCode.impl_getCode() + ", char: " + keyCode.impl_getChar() + "]");
         return sp.toString();
     }
 
